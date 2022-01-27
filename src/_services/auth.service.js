816ce6fcbbe7ede89a28e7ab79cf8d95cc0d075d
Yusuke Mizunuma <muzin00@gmail.com>
2022-01-27 16:28:48 +0900

    fix code style

--- src/_services/auth.service.js
+++ src/_services/auth.service.js
@@ -1,81 +1,87 @@
-import auth0 from 'auth0-js'
+import auth0 from 'auth0-js';
 
 export const authService = {
-    login,
-    signup,
-    changePassword
+  login,
+  signup,
+  changePassword,
 };
 
-function initWebAuth () {
-    var params = Object.assign({
-        domain: window['auth0Config'].auth0Domain,//removeProtocol(window['auth0Config'].authorizationServer.url),
-        clientID: window['auth0Config'].clientID,
-        redirectUri: window['auth0Config'].callbackURL,
-        audience: window['auth0Config'].audience,
-        responseType: window['auth0Config'].extraParams.response_type,
-        connection: window['auth0Config'].connection,
-        overrides: {
-            __tenant: window['auth0Config'].auth0Tenant,
-            __token_issuer: window['auth0Config'].authorizationServer.issuer
-        },
-    }, window['auth0Config'].internalOptions);
+function initWebAuth() {
+  var params = Object.assign(
+    {
+      domain: window['auth0Config'].auth0Domain, //removeProtocol(window['auth0Config'].authorizationServer.url),
+      clientID: window['auth0Config'].clientID,
+      redirectUri: window['auth0Config'].callbackURL,
+      audience: window['auth0Config'].audience,
+      responseType: window['auth0Config'].extraParams.response_type,
+      connection: window['auth0Config'].connection,
+      overrides: {
+        __tenant: window['auth0Config'].auth0Tenant,
+        __token_issuer: window['auth0Config'].authorizationServer.issuer,
+      },
+    },
+    window['auth0Config'].internalOptions
+  );
 
-    return new auth0.WebAuth(params);      
+  return new auth0.WebAuth(params);
 }
-      
-function login (username, password) {
-    return new Promise((resolve, reject) => {
-        try {
-            initWebAuth().login({
-                realm: window['auth0Config'].connection,
-                username: username,
-                password: password
-            }, err => {
-                console.log(err);
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        } catch (e) {
-            console.log(e);
-            reject(e);
+
+function login(username, password) {
+  return new Promise((resolve, reject) => {
+    try {
+      initWebAuth().login(
+        {
+          realm: window['auth0Config'].connection,
+          username: username,
+          password: password,
+        },
+        (err) => {
+          console.log(err);
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
         }
-    });       
+      );
+    } catch (e) {
+      console.log(e);
+      reject(e);
+    }
+  });
 }
 
 function signup(options) {
-    return new Promise((resolve, reject) => {
-        try {
-            initWebAuth().signup(options, err => {
-                console.log(err);
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        } catch (e) {
-            console.log(e);
-            reject(e);
+  return new Promise((resolve, reject) => {
+    try {
+      initWebAuth().signup(options, (err) => {
+        console.log(err);
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
         }
-    });     
+      });
+    } catch (e) {
+      console.log(e);
+      reject(e);
+    }
+  });
 }
 
 function changePassword(options) {
-    return new Promise((resolve, reject) => {
-        try {
-            initWebAuth().changePassword(options, (err, resp) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(resp);
-                }
-            });
-        } catch (e) {
-            console.log(e);
-            reject(e);
+  return new Promise((resolve, reject) => {
+    try {
+      initWebAuth().changePassword(options, (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(resp);
         }
-    });     
+      });
+    } catch (e) {
+      console.log(e);
+      reject(e);
+    }
+  });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,56 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
-    mode: 'development',
-    optimization: {
-        splitChunks: false,
-    },
-    resolve: {
-        extensions: ['.js', '.vue']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue?$/,
-                exclude: /(node_modules)/,
-                use: 'vue-loader'
+  mode: 'development',
+  optimization: {
+    splitChunks: false,
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue?$/,
+        exclude: /(node_modules)/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(scss|css)/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production',
             },
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(scss|css)/,
-                use: [
-                  "style-loader",
-                  "css-loader",
-                  {
-                    loader: "sass-loader",
-                    options: {
-                      sourceMap: process.env.NODE_ENV !== "production"
-                    }
-                  },
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+          },
+        ],
+      },
     ],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: 'http://localhost:4000',
+    }),
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
