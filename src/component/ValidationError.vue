aa9bae58f2051f6a7e711f1ea60b6c43ef1152f7
Yusuke Mizunuma <muzin00@gmail.com>
2022-01-28 16:30:42 +0900

    add new components

---
+++ src/component/ValidationError.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="error">{{ error }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.error {
+  margin-top: 8px;
+  color: #eb5757;
+}
+</style>
