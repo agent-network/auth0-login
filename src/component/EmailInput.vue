077b95ef3af044c23b1f4a77d1d943cc4986ad49
Yusuke Mizunuma <muzin00@gmail.com>
2022-02-04 11:32:47 +0900

    add EmailInput component

---
+++ src/component/EmailInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="email"
+    class="form-control"
+    :class="{ 'is-invalid': isInvalid }"
+    placeholder="メールアドレスを入力"
+    :value="value"
+    @input="onChange"
+  />
+</template>
+
+<script>
+import { fullWidthTohalfWidth } from '../lib/input';
+
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    isInvalid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onChange(e) {
+      const value = fullWidthTohalfWidth(e.target.value);
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
